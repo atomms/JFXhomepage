4a2ff2a74a4288e977dc11f718d98e26527dd19c
Ernesto Ramiro <atomms@gmail.com>
2017-11-07 12:43:32 +0100

    initial

---
+++ homepage/src/sample/Controller.java
@@ -0,0 +1,144 @@
+package sample;
+
+import com.sun.prism.Image;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller  implements Initializable{
+
+    @FXML
+    private AnchorPane pane1 ,pane2,pane3,pane4 , opacityPane,drawerPane;
+
+
+    @FXML
+    private ImageView drawerImage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        opacityPane.setVisible(false);
+
+        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(0.5),opacityPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.5),drawerPane);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+
+
+        pane1.setStyle("-fx-background-image: url(\"/sample/1.jpg\")");
+        pane2.setStyle("-fx-background-image: url(\"/sample/2.jpg\")");
+        pane3.setStyle("-fx-background-image: url(\"/sample/3.jpg\")");
+        pane4.setStyle("-fx-background-image: url(\"/sample/4.jpg\")");
+
+        Animation();
+
+
+        drawerImage.setOnMouseClicked(event -> {
+
+
+            opacityPane.setVisible(true);
+
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),opacityPane);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(0.15);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),drawerPane);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();
+        });
+
+        opacityPane.setOnMouseClicked(event -> {
+
+
+
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),opacityPane);
+            fadeTransition1.setFromValue(0.15);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(event1 -> {
+                opacityPane.setVisible(false);
+            });
+
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),drawerPane);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();
+        });
+
+    }
+
+    public  void  Animation(){
+
+
+        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(3),pane4);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        fadeTransition.setOnFinished(event -> {
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(3),pane3);
+            fadeTransition1.setFromValue(1);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(event1 -> {
+
+                FadeTransition fadeTransition2=new FadeTransition(Duration.seconds(3),pane2);
+                fadeTransition2.setFromValue(1);
+                fadeTransition2.setToValue(0);
+                fadeTransition2.play();
+
+                fadeTransition2.setOnFinished(event2 -> {
+
+                    FadeTransition fadeTransition00=new FadeTransition(Duration.seconds(3),pane2);
+                    fadeTransition00.setFromValue(0);
+                    fadeTransition00.setToValue(1);
+                    fadeTransition00.play();
+
+
+                    fadeTransition00.setOnFinished(event3 -> {
+                        FadeTransition fadeTransition11=new FadeTransition(Duration.seconds(3),pane3);
+                        fadeTransition11.setFromValue(0);
+                        fadeTransition11.setToValue(1);
+                        fadeTransition11.play();
+
+                        fadeTransition11.setOnFinished(event4 -> {
+                            FadeTransition fadeTransition22=new FadeTransition(Duration.seconds(3),pane4);
+                            fadeTransition22.setFromValue(0);
+                            fadeTransition22.setToValue(1);
+                            fadeTransition22.play();
+
+                            fadeTransition22.setOnFinished(event5 -> {
+                                Animation();
+                            });
+                        });
+
+
+                    });
+                });
+
+            });
+
+
+
+
+        });
+
+
+
+    }
+
+}
---
+++ homepage/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("sample.fxml"));
+        AnchorPane pane=loader.load();
+        Scene scene=new Scene(pane);
+        scene.getStylesheets().addAll(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setTitle("Home page");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
